310cec65e257f31d210b6e8bea795b5f7e8d1a02
DavidJoanes <dWX1344756@china.huawei.com>
2024-12-14 17:30:04 +0100

    subsequent

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2266994108"
+    serviceWorkerVersion: "430783933"
   }
 });
